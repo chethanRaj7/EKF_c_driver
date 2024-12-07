64a74c110d1fe751774298f0e21588d5d584bfde
chethanraj7 <rrajug24@gmail.com>
2024-12-08 05:06:04 +0530

    added support matrices operations and verified result

--- c_drivers/extended_KF.c
+++ c_drivers/extended_KF.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <math.h>
 
 
@@ -27,9 +26,11 @@ void choose_sim_variable(int variable, double value, int index, double *volt ,do
     switch (variable) {
     case 1:
         volt[index] = value;
+        //printf("voltage: %.15lf", value);
         break;
     case 2:
         current[index] = value;
+        //printf("current: %.15lf", value);
         break;
     case 3:
         //temp[index] = value;
@@ -118,9 +119,10 @@ void importmodel(FILE *fp, int num_lines, double *time,double *OCV, double *SOC,
 }
 
 double gen_measure(int index, double *volt, double *current){
+    //better not implement, read directly.
     double v = volt[index];
     double i = current[index];
-    return v, i;
+    return v, i;                                    //cannot return multiple variables like this, only returns I.
 }
 
 double calculateMeanDifference(double *SOC, int size) {
@@ -217,10 +219,84 @@ void importsim(FILE *fp, double *volt, double* current, double *discap, double *
     fclose(fp);
 }
 
+
+void dot(double mat1[3][3], double mat2[3][3], double res[3][3]){
+    //Multiply 2 3x3 matrices mutually.
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            res[i][j] = 0; // Initialize the result matrix cell
+            for (int k = 0; k < 3; k++) {
+                res[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+}
+
+void transpose(double mat[3][3], double transposed[3][3]) { 
+    //transpose an 3x3 matrix.
+    for (int i = 0; i < 3; i++) { 
+        for (int j = 0; j < 3; j++) { 
+            transposed[j][i] = mat[i][j]; 
+            }
+    }
+}
+
+void transpose3x1(double mat[1][3], double transposed[3][1]) { 
+    //transpose an 1x3 and 3x1
+    for (int i = 0; i < 1; i++) { 
+        for (int j = 0; j < 3; j++) { 
+            transposed[j][i] = mat[i][j]; 
+            } 
+    } 
+}
+
+
+void dot1x3(double mat1[1][3], double mat2[3][3], double res[1][3]) { 
+    for (int i = 0; i < 3; i++) { 
+        res[0][i] = 0; // Initialize the result matrix cell 
+        for (int j = 0; j < 3; j++) { 
+            res[0][i] += mat1[0][j] * mat2[j][i]; 
+        } 
+    }
+}
+
+void dot_product(double mat1[3][1], double mat2[3], double res[3][3]) { 
+    for (int i = 0; i < 3; i++) { 
+        for (int j = 0; j < 3; j++) { 
+            res[i][j] = mat1[i][0] * mat2[j]; 
+            } 
+    }
+}
+
+double dot1(double mat1[1][3], double mat2[3][1]) { 
+    double result = 0; 
+    for (int i = 0; i < 3; i++) { 
+        result += mat1[0][i] * mat2[i][0]; 
+    } 
+    return result; 
+}
+
+
+void add_scalar(double mat[3][3], double scalar) { 
+    for (int i = 0; i < 3; i++) { 
+        for (int j = 0; j < 3; j++) { 
+            mat[i][j] += scalar; 
+        } 
+    } 
+}
+
+void copy_matrix(double src[3][3], double dest[3][3]) { 
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) { 
+            dest[i][j] = src[i][j]; 
+        }
+    }
+}
+
 void main()
 {
     float xHat[] = {1.0, 0.0, 0.0};
-    float sigmaX[3][3] = {{1e-5, 0.0, 0.0},
+    double sigmaX[3][3] = {{1e-5, 0.0, 0.0},
                         {0.0, 1e-5, 0.0},
                         {0.0, 0.0, 1e-5}
                         };
@@ -229,12 +305,12 @@ void main()
     float iRC1Var = 1e1;
     float iRC2Var = 1e1;
 
-    float sigmaW[3][3] = {{socVar, 0.0, 0.0},
+    double sigmaW[3][3] = {{socVar, 0.0, 0.0},
                         {0.0, iRC1Var, 0.0},
                         {0.0, 0.0, iRC2Var}
                         };
 
-    float sigmaV = 1e-3;
+    double sigmaV = 1e-3;
     float socStore[] = {};
 
     //Import necessary datasets and initialize constant variables.
@@ -278,26 +354,88 @@ void main()
     double c2 = 129917.9637;
 
     float dt = 13.161842;
+    float eta = 1.0;
+    float capacityOCV = disCapacity[0];
 
     float RC1 = exp(-dt / (r1 * c1));
     float RC2 = exp(-dt / (r2 * c2));
     float V, I;
 
+    double Ahat[3][3] = { {1.0, 0.0, 0.0},
+                          {0.0, RC1, 0.0},
+                          {0.0, 0.0, RC2} };
+    
+    //double Bhat[3] = {(-dt * eta / (3600 * capacityOCV)), (1-RC1), (1-RC2)};
+    double Bhat[1][3] = {{(-dt * eta / (3600 * capacityOCV)), (1-RC1), (1-RC2)}};
+    double Chat[3] = {1.0, -r1, -r2};                           //replace the 1.0 during iteration.
+    double Dhat[1] = {1.0};
+
+
     double dZ = calculateMeanDifference(SOC, num_lines);        //constant, doesnt change.
     printf("dZ: %.15lf\n", dZ);
+    dZ = -0.0009115753413114999;
+
+    /*
     //dZ = -0.0009115753413114999;
-
-    //gen_measure()                                             //load the df befor this.
     for(int k =0;k< 10;k++){
-        float df = gendOCVSOC(k, SOC,OCV,num_lines,dZ );
-        printf("value: %f\n", df);
-    }
+        float df = gendOCVSOC(k, SOC, OCV, num_lines, dZ );
+        //printf("value: %f\n", df);                            //yields correct output when dZ is correct. Rest working fine.
+    } */
 
-    for(int iter = 0; iter < 10; iter++){
-        V, I = gen_measure(iter, volt, current);            //temporary, to be written.
+    //start simulation test.
+    double dOCVSOC;
+    double temp_ocv;
+    sim_num_lines = 4;
+
+    for(int iter = 0; iter < sim_num_lines; iter++){
+        //V, I = gen_measure(iter, volt, current);            //temporary, to be written.
+        V = volt[iter];
+        I = current[iter] * -1;
         printf("v: %.10lf, I: %.10lf, \n", V, I);
+
+        dOCVSOC = gendOCVSOC(xHat[0], SOC, OCV, num_lines, dZ);
+        printf("dOCVSOC %.10lf\n", dOCVSOC);
+        //OCV = voltOCV[argmin(abs(SOCOCV - xHat[0]))]
+        temp_ocv = volt[argmin(SOC, xHat[0], num_lines)];         //sharp drop's in OCV, check the argmin function.
+        printf("OCV %.10lf\n", temp_ocv);
+
+        xHat[0] = xHat[0] - (dt * I * eta / (3600 * capacityOCV));
+        xHat[1] = xHat[1] * RC1 + (1 - RC1) * I;
+        xHat[2] = xHat[2] * RC2 + (1 - RC2) * I;
+
+        // for( int i = 0; i<3; i++){
+        //     printf("%.10lf ", xHat[i]);
+        // }
+        
+        //sigmaX = np.dot( np.dot(Ahat, sigmaX), Ahat.T) + np.dot( np.dot(Bhat, sigmaW), Bhat.T)
+        double A_x[3][3];   dot(Ahat, sigmaX, A_x);
+
+        double A_t[3][3];   transpose(Ahat, A_t);                           //works perfectly fine.
+
+        double temp_mat1[3][3];     dot(A_x, A_t, temp_mat1);
+        
+        double B_w[1][3];       dot1x3(Bhat, sigmaW, B_w);                         //dot 1x3 and 3x3    res = 1x3
+
+        double B_t[3][1];       transpose3x1(Bhat, B_t);                           //transpose 1x3 bhat to 3x1
+        double scalar = dot1(B_w, B_t);
+        add_scalar(temp_mat1, scalar);
+
+        copy_matrix(temp_mat1, sigmaX);                                            //replace this fn later by passing pointer, instead of copy.
+
+        double yHat = ( temp_ocv - r0 * I - r1 * xHat[1] - r2 * xHat[2] );
+        printf("yhat: %.10lf\n", yHat);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                printf("%lf ", sigmaX[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+
         //compute the dOCVSOC.
     }
+   
 
     for(int k = 0; k< 1; k++){
         //printf("index: %d",argmin(SOC, ));
